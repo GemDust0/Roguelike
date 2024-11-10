cc0213d92ada99f1927460049f58612623eeccb5
GemDust <117678055+GemDust0@users.noreply.github.com>
2024-11-10 11:02:00 -0500

    Create Attack.java

---
+++ Roguelike/Attack.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class Attack {
+    
+    private String name;
+    private String description;
+    private ArrayList<Effect> attackTargetEffects;
+    private ArrayList<Effect> attackSelfEffects;
+    private boolean useTurn;
+    private Shop.Rarity rarity;
+    private int requirement;
+    
+    public Attack(String name, boolean useTurn, Effect[] attackTargetEffects, Effect[] attackSelfEffects, Shop.Rarity rarity, String description, int requirement){
+        this.name = name;
+        this.description = description;
+        this.useTurn = useTurn;
+        this.attackTargetEffects = new ArrayList<Effect>();
+        for (Effect effect : attackTargetEffects){
+            this.attackTargetEffects.add(effect);
+        }
+        this.attackSelfEffects = new ArrayList<Effect>();
+        for (Effect effect : attackSelfEffects){
+            this.attackSelfEffects.add(effect);
+        }
+        this.rarity = rarity;
+        this.requirement = requirement;
+    }
+    
+    public void addTargetEffect(Effect effect){
+        attackTargetEffects.add(effect);
+    }
+    
+    public void addSelfEffect(Effect effect){
+        attackSelfEffects.add(effect);
+    }
+    
+    public void use(Fighter user, Fighter target, BattleManager battle){
+        for (Effect effect : attackTargetEffects){
+            effect.apply(user, target, battle);
+            lib.clear();
+            battle.renderBattle();
+            lib.getInput();
+        }
+        for (Effect effect : attackSelfEffects){
+            effect.apply(target, user, battle);
+            lib.clear();
+            battle.renderBattle();
+            lib.getInput();
+        }
+    }
+    
+    public boolean usesTurn(){
+        return useTurn;
+    }
+    
+    public Shop.Rarity getRarity(){
+        return rarity;
+    }
+    
+    public int getRequirement(){
+        return requirement;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public boolean equals(Attack attack){
+        return name.equals(attack.getName());
+    }
+    
+    public String toString(){
+        return name + " - " + description;
+    }
+}
