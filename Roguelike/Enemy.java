c744e2056243d0ccbacee3565bcdf49f79ec4341
GemDust <117678055+GemDust0@users.noreply.github.com>
2024-11-15 18:25:29 -0500

    Update Enemy.java to V1.2

--- Roguelike/Enemy.java
+++ Roguelike/Enemy.java
@@ -7,9 +7,18 @@ public class Enemy extends Fighter{
         TANK
     }
     
-    public Enemy(Type type, int depth, boolean boss){
+    public void add(ArrayList<Integer> arraylist, int[] array){
+        for (int i : array){
+            arraylist.add(i);
+        }
+    }
+    
+    public Enemy(Type type, int depth, boolean boss, Player player){
         super(null, 0, 0);
         double healthMult = Math.pow(1.2, depth);
+        if (player.hasRelic(24)){
+            healthMult *= 1.5;
+        }
         int relicCount = depth;
         int extraAttackCount = depth*2;
         int energy = 5;
@@ -18,34 +27,78 @@ public class Enemy extends Fighter{
             healthMult *= 3;
             relicCount = (relicCount+1)*2;
             extraAttackCount = (extraAttackCount+1)*2;
-            addAttack(AttackList.attacks[8]);
             energy *= 2;
         }
         
         String[] availableNames = new String[] {};
-        int[] availableAttacks = new int[] {};
-        int[] availableRelics = new int[] {};
+        ArrayList<Integer> availableAttacks = new ArrayList<Integer>();
+        ArrayList<Integer> availableRelics = new ArrayList<Integer>();
         
         switch (type){
             case ATTACK:
                 availableNames = new String[] {"Atta"};
-                availableAttacks = new int[] {0, 0, 3, 3, 5, 6, 7};
-                availableRelics = new int[] {1};
+                
+                add(availableAttacks, new int[] {0, 0, 3, 3, 5, 6, 7});
+                availableRelics.add(1);
+                
+                if (depth >= 2){
+                    availableRelics.add(5);
+                }
+                
+                if (depth >= 3){
+                    add(availableAttacks, new int[] {5, 5, 8});
+                }
+                
+                if (depth >= 5){
+                    add(availableAttacks, new int[] {8, 8});
+                    availableAttacks.remove(0); // Remove the 2 strikes
+                    availableAttacks.remove(0);
+                    add(availableRelics, new int[] {5, 8});
+                }
+                
+                if (depth >= 8) {
+                    add(availableRelics, new int[] {8, 8, 8, 8});
+                }
+                
                 setMaxHealth((int)(15*healthMult));
                 addAttack(AttackList.attacks[0]);
                 addAttack(AttackList.attacks[0]);
                 addAttack(AttackList.attacks[5]);
                 break;
+            
             case TANK:
                 availableNames = new String[] {"Amor"};
-                availableAttacks = new int[] {0, 0, 1, 2, 2, 2, 3, 9, 9, 9};
-                availableRelics = new int[] {1, 4, 4, 4, 12, 13};
+                
+                add(availableAttacks, new int[] {0, 0, 1, 2, 2, 2, 3, 9, 9, 9});
+                add(availableRelics, new int[] {1, 4, 4, 4, 20, 20, 21});
+                
+                if (depth >= 2){
+                    add(availableRelics, new int[] {6, 6, 6, 7, 21, 21, 22});
+                }
+                
+                if (depth >= 5){
+                    add(availableAttacks, new int[] {8, 8});
+                    availableAttacks.remove(0); // Remove Strikes
+                    availableAttacks.remove(0);
+                    add(availableRelics, new int[] {9, 9, 10, 22, 22});
+                    availableRelics.remove(1); // Remove Chicken Wings
+                    availableRelics.remove(1);
+                    availableRelics.remove(1);
+                }
+                
+                if (depth >= 9){
+                    add(availableRelics, new int[] {23, 23, 23, 23});
+                    availableRelics.remove(1); // Remove Hot Dogs
+                    availableRelics.remove(1);
+                }
+                
                 setMaxHealth((int)(20*healthMult));
                 addAttack(AttackList.attacks[0]);
                 addAttack(AttackList.attacks[2]);
                 addAttack(AttackList.attacks[2]);
                 addAttack(AttackList.attacks[9]);
                 break;
+            
             default:
                 availableNames = new String[] {"Null"};
                 setMaxHealth((int)(9999*healthMult));
@@ -53,8 +106,12 @@ public class Enemy extends Fighter{
         
         setName(availableNames[lib.randint(availableNames.length)]);
         
+        if (boss){
+            addAttack(AttackList.attacks[8]);
+        }
+        
         for (int i=0; i<extraAttackCount; i++){
-            addAttack(AttackList.attacks[availableAttacks[lib.randint(availableAttacks.length)]]);
+            addAttack(AttackList.attacks[availableAttacks.get(lib.randint(availableAttacks.size()))]);
         }
         
         for (int i=0; i<relicCount; i++){
