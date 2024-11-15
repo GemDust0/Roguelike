a5612112bf2c10d031a5c5f1d1fe1421d2ba955b
GemDust <117678055+GemDust0@users.noreply.github.com>
2024-11-15 18:23:58 -0500

    Update AttackList.java to V1.2

--- Roguelike/AttackList.java
+++ Roguelike/AttackList.java
@@ -8,7 +8,10 @@ public class AttackList {
         new Attack("War Cry", true, new Effect[] {}, new Effect[] {new Effect(Effect.EffectEnum.STRENGTH, null, 4)}, Shop.Rarity.COMMON, "Gain strength 4", -1),
         new Attack("Quick Jab", false, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 10)}, new Effect[] {}, Shop.Rarity.UNCOMMON, "Deal 10 damage, does not use turn", -1),
         new Attack("Poison Jab", true, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 15), new Effect(Effect.EffectEnum.POISON, 1, 5, 50)}, new Effect[] {}, Shop.Rarity.UNCOMMON, "Deal 15 damage, may poison", -1),
-        new Attack("Megastrike", true, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 40)}, new Effect[] {}, Shop.Rarity.RARE, "Deal 40 damage", 0),
-        new Attack("Sacrificial Strike", true, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 30)}, new Effect[] {new Effect(Effect.EffectEnum.PERCENTDAMAGE, 1, 30)}, Shop.Rarity.UNCOMMON, "Sacrifice 30% of your health to deal 30 damage", -1)
+        new Attack("Megastrike", true, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 30)}, new Effect[] {}, Shop.Rarity.RARE, "Deal 30 damage", 0),
+        new Attack("Sacrificial Strike", true, new Effect[] {new Effect(Effect.EffectEnum.DAMAGE, 1, 20)}, new Effect[] {new Effect(Effect.EffectEnum.PERCENTDAMAGE, 1, 25)}, Shop.Rarity.UNCOMMON, "Sacrifice 25% of your health to deal 20 damage", -1),
+        new Attack("Fireball", true, new Effect[] {new Effect(Effect.EffectEnum.TRUEDAMAGE, 1, 10)}, new Effect[] {}, Shop.Rarity.COMMON, "Deal 10 true damage", -1),
+        new Attack("Thunderbolt", true, new Effect[] {new Effect(Effect.EffectEnum.TRUEDAMAGE, 1, 20)}, new Effect[] {}, Shop.Rarity.RARE, "Deal 30 true damage", -1),
+        new Attack("Thunder", true, new Effect[] {new Effect(Effect.EffectEnum.TRUEDAMAGE, 1, 40)}, new Effect[] {}, Shop.Rarity.LEGENDARY, "Deal 50 true damage", -1)
     };
 }
